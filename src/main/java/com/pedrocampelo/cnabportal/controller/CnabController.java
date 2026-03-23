05b42e76730a42a65779f5a359107cb8c2895128
PedroRCampelo <pedrocampelo@live.com>
2026-03-23 14:56:23 -0300

    adj cors

--- src/main/java/com/pedrocampelo/cnabportal/controller/CnabController.java
+++ src/main/java/com/pedrocampelo/cnabportal/controller/CnabController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://portalcnab-frontend.onrender.com/"
+        "https://portalcnab-frontend.onrender.com"
 })
 @RestController
 @RequestMapping("/api/cnab")
